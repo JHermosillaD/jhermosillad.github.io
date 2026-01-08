711941208e1e87ec665360143353376d81a9d519
Jesus Hermosilla <hermosilladiaz@gmail.com>
2026-01-08 00:57:20 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
@@ -9,14 +8,6 @@ export default defineConfig({
   site: 'https://jhermosillad.github.io',
   integrations: [
     mdx(),
-    sitemap({
-      serialize(item) {
-        if (item.url.endsWith('/rss.xml')) {
-          return undefined;
-        }
-        return item;
-      }
-    }),
     tailwind()
   ],
   markdown: {
